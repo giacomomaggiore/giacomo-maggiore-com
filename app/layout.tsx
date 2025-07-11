e21c1561c4d48203190ae5b64fa717c895d1a3e0
giacomo.maggiore <giaco.maggiore@gmail.com>
2025-07-11 16:59:29 +0200

    ooved google tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -61,6 +61,7 @@ export default function RootLayout({
         GeistMono.variable
       )}
     >
+      <GoogleAnalytics gaId="G-6E5X8EMEPL" />
       <body className="antialiased max-w-2xl mx-4 mt-8 lg:mx-auto">
           <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
             <Navbar />
@@ -68,7 +69,7 @@ export default function RootLayout({
             <Footer />
           </main>
       </body>
-      <GoogleAnalytics gaId="G-6E5X8EMEPL" />
+      
     </html>
   )
 }
