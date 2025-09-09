144a71cd582cc17cc9ff482d10807202db03f3af
giacomo.maggiore <giaco.maggiore@gmail.com>
2025-09-09 09:30:13 +0200

    chapter 5 utilità attesa done

--- app/page.tsx
+++ app/page.tsx
@@ -9,13 +9,12 @@ export default function Page() {
       <p className="mb-2">
       I was born and raised in <a href='https://it.wikipedia.org/wiki/Milano' target='_blank'><b>Milan</b></a>, and since I was a child, I’ve had a 
       strong passion for numbers, technology and physics. 
-      <br></br><br></br>
+      <br></br>
       After high school, I studied <a target='_blank' href='https://www.polimi.it/formazione/corsi-di-laurea/dettaglio-corso/ingegneria-dellautomazione'><i>Automation Engineering</i> </a>
        at <a href='https://www.polimi.it/' target='_blank'><b>Politecnico di Milano</b></a>,
-      focusing my academic path on the most quantitative subjects and
       spending one semester abroad in Sweden, as an exchange student
        at <a href='https://www.mdu.se/en/malardalen-university' target="_blank"><b> Mälardalen University</b></a>.
-      <br></br><br></br>
+<br></br><br></br>
     Shortly after, I co-founded <a href="https://scenesnap.co/" target='_blank'><b>SceneSnap</b></a>, 
     a B2B and B2C EdTech startup 
     that develops learning tools by bridging artificial intelligente and data analytics.
@@ -26,14 +25,15 @@ export default function Page() {
     <br></br><br></br>
     Then, I decided to shift my future studies in this direction, reason why I left Polimi and my startup 
     to start a new adventure in <b>Zürich</b>, where I’m currently pursuing a Master in <b><a href="https://mtec.ethz.ch/studies.html" target="_blank">Economnics, Management & Technology</a></b>,
-    focusing on finane, quantitative methods and asset management.
+    focusing on finance, quantitative methods and asset management.
     <br></br><br></br>
     When I'm not working or studying, you can find me running and cycling.
+    <br></br>
     In 2024 I ran <a target='_blank' href='https://www.youtube.com/watch?v=vIGYlnaqcP8&t=451s'><b>my first marathon</b> </a>
     and completed my first ultramarathon, sharing my 
     journey on <a href='https://www.youtube.com/@giacomomaggiore01' target='_blank'><b>YouTube</b></a> and <a href='https://www.strava.com/athletes/24804406' target='_blank'><b>Strava</b></a>.
     
-    <br></br><br></br>
+    <br></br>
     Beyond running, I enjoy outdoor activities and love to travel 
     in a rough and itinerant way, always looking for unusual and cheap destinations, 
     far from tourist hotspots.
