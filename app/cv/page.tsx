d8739c06040c20a863d4f4abdc66c54268746973
giacomo.maggiore <giaco.maggiore@gmail.com>
2025-09-10 12:14:01 +0200

    fix cv

--- app/cv/page.tsx
+++ app/cv/page.tsx
@@ -2,13 +2,13 @@
 export default function CVPage() {
   return (
     <div className="prose mx-auto p-4">
-      <h1 className="font-semibold text-2xl mb-8 tracking-tighter">Giacomo's curriculum vitae</h1>
+      <h1 className="font-semibold text-2xl mb-8 tracking-tighter">Giacomo's Curriculum Vitae</h1>
       <iframe
-        src="/cv.pdf"
+        src="/cv_giacomo_maggiore.pdf"
         width="100%"
         height="800px"
         className="border"
-        title="CV di Giacomo"
+        title="Giacomo's Curriculum Vitae"
       />
     </div>
   );
