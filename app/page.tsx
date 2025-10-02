f13f0ed062efab4688b1cd32195eb95f88ff8fad
giacomo.maggiore <giaco.maggiore@gmail.com>
2025-10-02 14:58:29 +0200

    bio update

--- app/page.tsx
+++ app/page.tsx
@@ -10,31 +10,30 @@ export default function Page() {
       I was born and raised in <a href='https://it.wikipedia.org/wiki/Milano' target='_blank'><b>Milan</b></a>, and since I was a child, I’ve had a 
       strong passion for numbers, technology and physics. 
       <br></br>
-      After high school, I studied <a target='_blank' href='https://www.polimi.it/formazione/corsi-di-laurea/dettaglio-corso/ingegneria-dellautomazione'><i>Automation Engineering</i> </a>
+      After high school, I studied <a target='_blank' href='https://www.polimi.it/formazione/corsi-di-laurea/dettaglio-corso/ingegneria-dellautomazione'><u><i>Automation Engineering</i></u> </a>
        at <a href='https://www.polimi.it/' target='_blank'><b>Politecnico di Milano</b></a>,
       spending one semester abroad in Sweden, as an exchange student
        at <a href='https://www.mdu.se/en/malardalen-university' target="_blank"><b> Mälardalen University</b></a>.
 <br></br><br></br>
-    Shortly after, I co-founded <a href="https://scenesnap.co/" target='_blank'><b>SceneSnap</b></a>, 
-    a B2B and B2C EdTech startup 
+    Shortly after, I co-founded <a href="https://scenesnap.co/" target='_blank'><u><i>SceneSnap</i></u></a>, 
+    a B2B and B2C <b>EdTech startup </b>
     that develops learning tools by bridging artificial intelligente and data analytics.
     <br></br><br></br>
-      During my studies, I deep dived into love with <b>financial markets</b> and <b>personal finance</b>, 
-      reason why <a href='https://giacomomaggiore.com/blog/01-polimi-recap' target='_blank'>I left Polimi </a>
-      and my startup to start a new adventure at <u><a href="https://mtec.ethz.ch/studies.html" target='_blank'>
-    ETH Zurich</a></u>, where I’m currently pursuing a Master in <b><a href="https://mtec.ethz.ch/studies.html" target="_blank">Economnics, Management & Technology</a></b>,
+      During my studies, I deep dived into <b>financial markets</b> and <b>personal finance</b>, 
+      reason why I  moved to <i><u><a href="https://mtec.ethz.ch/studies.html" target='_blank'>
+    ETH Zurich</a></u></i>, where I’m currently pursuing a Master in <b><a href="https://mtec.ethz.ch/studies.html" target="_blank">Economnics, Management & Technology</a></b>,
         focusing on the most quantitative and analytically oriented courses.
     <br></br><br></br>
     I’m deeply interested in <b>financial risk management</b>, <b>quantitative </b>and<b> personal finance</b>. 
-    Alongside my studies, I work as a research assistant at <a href="https://kof.ethz.ch/it/" target="_blank"><u>KOF, the Swiss Economic Institute</u></a> at ETH Zurich, 
+    Alongside my studies, I work as a research assistant at <a href="https://kof.ethz.ch/it/" target="_blank"><u><i>KOF</i></u>, the Swiss Economic Institute</a>, 
     where I further strengthen my analytical and research skills.
 
     <br></br><br></br>
-    When I'm not working or studying, you can find me running and cycling.
+    When I'm not working or studying, you can find me <b>running</b> and <b>cycling</b>.
     <br></br>
-    In 2024 I ran <a target='_blank' href='https://www.youtube.com/watch?v=vIGYlnaqcP8&t=451s'><b>my first marathon</b> </a>
+    In 2024 I ran <a target='_blank' href='https://www.youtube.com/watch?v=vIGYlnaqcP8&t=451s'><i><u>my first marathon</u></i> </a>
     and completed my first ultramarathon, sharing my 
-    journey on <a href='https://www.youtube.com/@giacomomaggiore01' target='_blank'><b>YouTube</b></a> and <a href='https://www.strava.com/athletes/24804406' target='_blank'><b>Strava</b></a>.
+    journey on <a href='https://www.youtube.com/@giacomomaggiore01' target='_blank'><i><u>YouTube</u></i></a> and <a href='https://www.strava.com/athletes/24804406' target='_blank'><i><u>Strava</u></i></a>.
     
     <br></br>
     Beyond running, I enjoy outdoor activities and love to travel 
