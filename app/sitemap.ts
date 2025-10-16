d9ef786608ffdb9a83a90e5ab83852d1b2e51cae
giacomo.maggiore <giaco.maggiore@gmail.com>
2025-10-16 12:24:44 +0200

    sitemap fix maybe

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,17 +1,18 @@
-import { getBlogPosts } from 'app/blog/utils'
-
-export const baseUrl = 'https://portfolio-blog-starter.vercel.app'
+import { getBlogPosts } from './blog/utils'
+import { baseUrl } from './sitemap'
 
 export default async function sitemap() {
-  let blogs = getBlogPosts().map((post) => ({
-    url: `${baseUrl}/blog/${post.slug}`,
-    lastModified: post.metadata.publishedAt,
-  }))
+  const langs = ['it', 'en']
+  let blogs: { url: string; lastModified: string }[] = []
 
-  let routes = ['', '/blog'].map((route) => ({
-    url: `${baseUrl}${route}`,
-    lastModified: new Date().toISOString().split('T')[0],
-  }))
+  for (const lang of langs) {
+    blogs.push(
+      ...getBlogPosts(lang as 'it' | 'en').map((post) => ({
+        url: `${baseUrl}/blog/${lang}/${post.slug}`,
+        lastModified: post.metadata.publishedAt,
+      }))
+    )
+  }
 
-  return [...routes, ...blogs]
+  return blogs
 }
