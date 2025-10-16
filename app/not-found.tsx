3f3ed2cc99816bf6ca551493cccc1eb1d2949d02
giacomo.maggiore <giaco.maggiore@gmail.com>
2025-10-16 18:18:58 +0200

    rss feed in english and update 404

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,9 +2,16 @@ export default function NotFound() {
   return (
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        404 - Page Not Found
+        404
       </h1>
-      <p className="mb-4">The page you are looking for does not exist.</p>
+      <p className="mb-4">Either you're wrong, or I (Giacomo) designed this website like a maze with zero signs.</p>
+
+      <a href="https://giacomomaggiore.com/" className=" hover:underline">
+      <b>
+      
+      Please come back in the safe area!
+      </b>
+      </a>
     </section>
   )
 }
