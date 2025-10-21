d6fb5d2724352629efb5f2f22ed87326c49451af
giacomo.maggiore <giaco.maggiore@gmail.com>
2025-10-21 21:07:23 +0200

    blog post component fix

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -9,14 +9,12 @@ export default async function sitemap() {
   const langs = ['it', 'en']
   let blogs: { url: string; lastModified: string }[] = []
 
-  for (const lang of langs) {
-    blogs.push(
-      ...getBlogPosts(lang as 'it' | 'en').map((post) => ({
-        url: `${baseUrl}/blog/${lang}/${post.slug}`,
-        lastModified: post.metadata.publishedAt,
-      }))
-    )
-  }
+  blogs.push(
+    ...getBlogPosts().map((post) => ({
+      url: `${baseUrl}/blog/it/${post.slug}`,
+      lastModified: post.metadata.publishedAt,
+    }))
+  )
 
   return blogs
 }
