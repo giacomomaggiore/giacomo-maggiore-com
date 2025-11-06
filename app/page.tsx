b5e375c2a2fee0872599b102a16b61a4eb30efd3
giacomo.maggiore <giaco.maggiore@gmail.com>
2025-11-06 11:08:08 +0100

    modified bio

--- app/page.tsx
+++ app/page.tsx
@@ -5,39 +5,20 @@ export default function Page() {
         Giacomo Maggiore
       </h1>
       <p className="mb-2">
-      I was born and raised in <a href='https://it.wikipedia.org/wiki/Milano' target='_blank'><b>Milan</b></a>, and since I was a child, I’ve had a 
-      strong passion for numbers, technology and physics. 
-      <br></br>
-      After high school, I studied <a target='_blank' href='https://www.polimi.it/formazione/corsi-di-laurea/dettaglio-corso/ingegneria-dellautomazione'><u><i>Automation Engineering</i></u> </a>
-       at <a href='https://www.polimi.it/' target='_blank'><b>Politecnico di Milano</b></a>,
-      spending one semester abroad in Sweden, as an exchange student
-       at <a href='https://www.mdu.se/en/malardalen-university' target="_blank"><b> Mälardalen University</b></a>.
+      Born and raised in <a href='https://it.wikipedia.org/wiki/Milano' target='_blank'><b>Milan</b></a>, I studied <a href='https://www.polimi.it/formazione/corsi-di-laurea/dettaglio-corso/ingegneria-dellautomazione'><u><i>Automation Engineering</i></u> </a>
+       at <a href='https://www.polimi.it/' target='_blank'><b>Polimi</b></a>, spending one semester abroad in Sweden     at <a href='https://www.mdu.se/en/malardalen-university' target="_blank"><u> <i>MDU</i></u></a>.
 <br></br><br></br>
-    Shortly after, I co-founded <a href="https://scenesnap.co/" target='_blank'><u><i>SceneSnap</i></u></a>, 
-    a B2B and B2C <b>EdTech startup </b>
-    that develops learning tools by bridging artificial intelligente and data analytics.
+    I've co-founded <a href="https://scenesnap.co/" target='_blank'><u><i>SceneSnap</i></u></a>, 
+    a B2B and B2C <b>EdTech startup </b> building an AI-powered learning ecosystem.
     <br></br><br></br>
-      During my studies, I deep dived into <b>financial markets</b> and <b>personal finance</b>, 
-      reason why I  moved to <i><u><a href="https://mtec.ethz.ch/studies.html" target='_blank'>
-    ETH Zurich</a></u></i>, where I’m currently pursuing a Master in <b><a href="https://mtec.ethz.ch/studies.html" target="_blank">Economnics, Management & Technology</a></b>,
-        focusing on the most quantitative and analytically oriented courses.
-    <br></br><br></br>
-    I’m deeply interested in <b>financial risk management</b>, <b>quantitative </b>and<b> personal finance</b>. 
-    Alongside my studies, I work as a research assistant at <a href="https://kof.ethz.ch/it/" target="_blank"><u><i>KOF</i></u>, the Swiss Economic Institute</a>, 
-    where I further strengthen my analytical and research skills.
+    I'm now pursuing a Master in <b><a href="https://mtec.ethz.ch/studies.html" target="_blank">Economnics, Management & Technology</a></b> at <a href="https://ethz.ch/en.html" target="_blank"><u><i>ETHZ</i></u></a>, and working as a research assistant at <a href="https://kof.ethz.ch/it/" target="_blank"><u><i>KOF (Swiss Economic Institute)</i></u></a>.
 
     <br></br><br></br>
-    When I'm not working or studying, you can find me <b>running</b> and <b>cycling</b>.
-    <br></br>
-    In 2024 I ran <a target='_blank' href='https://www.youtube.com/watch?v=vIGYlnaqcP8&t=451s'><i><u>my first marathon</u></i> </a>
-    and completed my first ultramarathon, sharing my 
-    journey on <a href='https://www.youtube.com/@giacomomaggiore01' target='_blank'><i><u>YouTube</u></i></a> and <a href='https://www.strava.com/athletes/24804406' target='_blank'><i><u>Strava</u></i></a>.
+    I’m interested in how<b> numbers model money</b>: financial risk, macroeconomic forecasting, and both quantitative and personal finance
+    <br></br><br></br>
+
+    Outside work and study, you can find me  running, cycling and exploring unconventional, low-budget travel destinations.
     
-    <br></br>
-    Beyond running, I enjoy outdoor activities and love to travel 
-    in a rough and itinerant way, always looking for unusual and cheap destinations, 
-    far from tourist hotspots.
-
       </p>
       <div className="my-8">
       </div>
